e0bbb19d738c65b8cbe4ad6298b272a1f4048524
Ben Pettit <benpettit@digimulti.com>
2012-08-03 18:27:24 +1000

    had to do another re-sort of directory structures since apparently github overwrites the source variable, so I need to have all the source files in the root path.

---
+++ _build/_Digimulti Website.build/Debug/Jekyll Local Server.build/Script-EB697DE315CB945700AF959F.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export PATH="/Users/eggers/.rvm/gems/ruby-1.9.2-p290/bin:/Users/eggers/.rvm/gems/ruby-1.9.2-p290@global/bin:/Users/eggers/.rvm/rubies/ruby-1.9.2-p290/bin:/Users/eggers/.rvm/bin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin:/Users/eggers/Dropbox/software dev/misc:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+export GEM_HOME="/Users/eggers/.rvm/gems/ruby-1.9.2-p290"
+export GEM_PATH="/Users/eggers/.rvm/gems/ruby-1.9.2-p290:/Users/eggers/.rvm/gems/ruby-1.9.2-p290@global"
+jekyll --server 1337 --auto
